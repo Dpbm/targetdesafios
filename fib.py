54beaaf7ceb2b1d4b7b0d9a68b781eb56c12ff9a
Dpbm <dpbm136@gmail.com>
2024-09-24 15:38:46 -0300

    feat: fib code

---
+++ fib.py
@@ -0,0 +1,41 @@
+memo = {}
+def fib(i):
+    if(i <= 0):
+        return 0
+    elif(i == 1):
+        return 1
+    elif(memo.get(i) != None):
+        return memo[i]
+
+    fib_1 = fib(i-1)
+    fib_2 = fib(i-2)
+
+    if(memo.get(i-1) == None):
+       memo[i-1] = fib_1
+    if(memo.get(i-2) == None):
+        memo[i-2] = fib_2
+
+    return fib_1 + fib_2
+
+def find_n(target):
+    if(target in {0, 1}):
+        print("valor pertence a sequência!")
+        return
+
+    counter = 3
+    while True:
+        fib_value = fib(counter)
+        if(fib_value == target):
+            print("Valor pertence a sequência")
+            return
+        elif(fib_value > target):
+            print("Valor não pertence a sequência!")
+            return
+
+        counter += 1
+
+
+
+if __name__ == '__main__':
+    target = int(input("Valor que deseja encontrar: "))
+    find_n(target)
